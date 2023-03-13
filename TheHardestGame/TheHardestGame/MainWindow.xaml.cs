f60b80b1a7d0bc25fc34c20f7ab74dded7b748ee
Maxi <max.03.strigl@hotmail.com>
2023-03-13 17:28:32 +0100

    still wanna kill myself, added Collsion, Enemys, Death

--- TheHardestGame/TheHardestGame/MainWindow.xaml.cs
+++ TheHardestGame/TheHardestGame/MainWindow.xaml.cs
@@ -28,17 +28,23 @@ namespace TheHardestGame
         int tickSpeed = 16;
 
         float speedY, speedX;
-
         float speed = 2;
-
-        int test = 3;
-
         bool up = false, down = false, left = false, right = false;
+
+
+        float enemySpeed = 6;
+        float firstSpeed;
+        float secondSpeed;
+        float thirdSpeed;
+        float fourthSpeed;
 
         public MainWindow()
         {
             InitializeComponent();
             Canvas.Focus();
+
+            firstSpeed = secondSpeed = thirdSpeed = fourthSpeed = enemySpeed;
+
 
             MovementTimer = new DispatcherTimer(DispatcherPriority.Send);
 
@@ -53,7 +59,28 @@ namespace TheHardestGame
 
         void GameTick(object? sender, EventArgs e)
         {
-            
+            MoveEnemys();
+        }
+
+        private void MoveEnemys()
+        {
+            Collide();
+
+            Canvas.SetLeft(FirstRow, Canvas.GetLeft(FirstRow) + firstSpeed * -0.88f);
+            if (Canvas.GetLeft(FirstRow) < 212) firstSpeed *= -1;
+            if (Canvas.GetLeft(FirstRow) > 572) firstSpeed *= -1;
+
+            Canvas.SetLeft(SecondRow, Canvas.GetLeft(SecondRow) + secondSpeed * 0.88f);
+            if (Canvas.GetLeft(SecondRow) < 212) secondSpeed *= -1;
+            if (Canvas.GetLeft(SecondRow) > 572) secondSpeed *= -1;
+
+            Canvas.SetLeft(ThirdRow, Canvas.GetLeft(ThirdRow) + thirdSpeed * -0.88f);
+            if (Canvas.GetLeft(ThirdRow) < 212) thirdSpeed *= -1;
+            if (Canvas.GetLeft(ThirdRow) > 572) thirdSpeed *= -1;
+
+            Canvas.SetLeft(FourthRow, Canvas.GetLeft(FourthRow) + fourthSpeed * 0.88f);
+            if (Canvas.GetLeft(FourthRow) < 212) fourthSpeed *= -1;
+            if (Canvas.GetLeft(FourthRow) > 572) fourthSpeed *= -1;
         }
 
         private void Movement(object? sender, EventArgs e)
@@ -78,6 +105,32 @@ namespace TheHardestGame
             CollideY();
         }
 
+        private void Collide()
+        {
+            Rect PlayerHB = new Rect(Canvas.GetLeft(Player), Canvas.GetTop(Player), Player.Width, Player.Height);
+
+            foreach(var x in Canvas.Children.OfType<Ellipse>())
+            {
+                if((string)x.Tag == "enemy")
+                {                   
+                    Rect collisionHB = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+
+                    if (PlayerHB.IntersectsWith(collisionHB))
+                    {
+                        GameOver();
+                        return;
+                    }
+                }
+            }
+
+        }
+
+        private void GameOver()
+        {
+            DP.Stop();
+            ResultLabel.Content = "LOL Looser \n click 'R' to restart";
+            
+        }
         private void CollideX()
         {
             Rect PlayerHB = new Rect(Canvas.GetLeft(Player), Canvas.GetTop(Player), Player.Width, Player.Height);
@@ -140,9 +193,18 @@ namespace TheHardestGame
                 case Key.A:
                     left = true;
                     break;
+
+                case Key.R:
+                    resetGame();
+                    break;
             }
             MovementTimer.Start();
             Movement(sender, e);
+        }
+
+        private void resetGame()
+        {
+            Canvas.SetLeft(Player, 87); Canvas.SetTop(Player, 209);
         }
 
         private void ButtonReleased(object sender, KeyEventArgs e)
